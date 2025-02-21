bd3d550ad45be23adba24fef18b1febef73a566e
usamahdhaqi <usamahdhaqi@gmail.com>
2025-02-21 20:23:13 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " adalah bilangan genap.");
+    } else {
+        console.log(i + " adalah bilangan ganjil.");
+    }
+}
